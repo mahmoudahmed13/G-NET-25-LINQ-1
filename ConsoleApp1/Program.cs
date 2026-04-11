53b775ec272390ba7550b9966afeffe82e9e51d8
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-04-12 00:55:29 +0200

    Question 01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace ConsoleApp1
+﻿using static LINQ.DataSources.Source;
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Question 01
+            ////1. Get all products from the "Seafood" category. Print each product's name and price.
+
+            //var result = ProductList.Where(p => p.Category == "Seafood")
+            //                        //.Select(p => $"Name: {p.ProductName}, Price: {p.UnitPrice}");
+            //                        .Select(p => new
+            //                        {
+            //                            Name = p.ProductName,
+            //                            Price = p.UnitPrice
+            //                        });
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
+
+            #endregion
         }
     }
 }
